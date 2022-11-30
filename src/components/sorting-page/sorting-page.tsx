912c5e1d783cad877594be0639cc1ce661c4ad1c
Alexander <sashashvetsov3@gmail.com>
2022-11-30 22:04:15 +0300

    add select sort logic

--- src/components/sorting-page/sorting-page.tsx
+++ src/components/sorting-page/sorting-page.tsx
@@ -6,45 +6,105 @@ import { Column } from '../ui/column/column';
 import { RadioInput } from '../ui/radio-input/radio-input';
 import { SolutionLayout } from '../ui/solution-layout/solution-layout';
 import styles from './sorting.module.css';
-import { generateRandomArr, maxLen, minLen, randomNumber } from './utils/utils';
+import { generateRandomArr, maxLen, minLen, randomNumber, TArrWithState } from './utils/utils';
 import { nanoid } from 'nanoid';
-
-interface IArrItem {
-	number: number;
-	state: ElementStates;
-}
+import { animationDelay } from '../../utils/utils';
 
 export const SortingPage: React.FC = () => {
-	const [checked, setChecked] = useState(false);
-	const [arr, setArr] = useState<IArrItem[]>([]);
-  console.log(arr)
+	const [isBubble, setIsBubble] = useState(false);
+	const [arrWithState, setArrWithState] = useState<TArrWithState[]>([]);
+
+	const handleClickNewArrWithState = () => {
+		setArrWithState(
+			generateRandomArr(randomNumber(minLen, maxLen)).reduce((acc: TArrWithState[], item) => {
+				return [...acc, { number: item, state: ElementStates.Default }];
+			}, []),
+		);
+	};
+
+	const selectionSort = async (inputArr: TArrWithState[], direction: string) => {
+		console.log('cicle', direction);
+		for (let i = 0; i < inputArr.length; i++) {
+			let min = i;
+			inputArr[min].state = ElementStates.Changing;
+			for (let j = i + 1; j < inputArr.length; j++) {
+				inputArr[j].state = ElementStates.Changing;
+				setArrWithState([...inputArr]);
+				await animationDelay(200);
+				if (
+					direction === 'desc'
+						? inputArr[j].number > inputArr[min].number
+						: inputArr[j].number < inputArr[min].number
+				) {
+					min = j;
+				}
+				inputArr[j].state = ElementStates.Default;
+				setArrWithState([...inputArr]);
+			}
+			if (min !== i) {
+				inputArr[min].state = ElementStates.Modified;
+				inputArr[i].state = ElementStates.Default;
+				[inputArr[i], inputArr[min]] = [inputArr[min], inputArr[i]];
+			} else {
+				inputArr[i].state = ElementStates.Modified;
+			}
+			setArrWithState([...inputArr]);
+		}
+
+		return inputArr;
+	};
 
 	useEffect(() => {
-		setArr([...generateRandomArr(randomNumber(minLen, maxLen))]);
+		handleClickNewArrWithState();
 	}, []);
 
-	const handleClickNewArr = () => {
-		setArr([...generateRandomArr(randomNumber(minLen, maxLen))]);
+	const handleChange = () => {
+		setIsBubble(!isBubble);
 	};
 
-	const handleChange = () => {
-		setChecked(!checked);
+	const handleClickSort = async (direction: string): Promise<void> => {
+		setArrWithState(await selectionSort(arrWithState, direction));
 	};
+
 	return (
 		<SolutionLayout title='Сортировка массива'>
 			<form className={styles.form}>
 				<div className={styles.radioInput}>
-					<RadioInput label='Выбор' checked={!checked} onChange={handleChange} />
-					<RadioInput label='Пузырёк' checked={checked} onChange={handleChange} />
+					<RadioInput
+						label='Выбор'
+						checked={!isBubble}
+						onChange={handleChange}
+						value='select-sort'
+					/>
+					<RadioInput
+						label='Пузырёк'
+						checked={isBubble}
+						onChange={handleChange}
+						value='bubble-sort'
+					/>
 				</div>
 				<div className={styles.buttonsSort}>
-					<Button text='По возрастанию' extraClass={styles.btnSize} sorting={Direction.Ascending} />
-					<Button text='По убыванию' extraClass={styles.btnSize} sorting={Direction.Descending} />
+					<Button
+						text='По возрастанию'
+						extraClass={styles.btnSize}
+						sorting={Direction.Ascending}
+						onClick={() => handleClickSort('asc')}
+					/>
+					<Button
+						text='По убыванию'
+						extraClass={styles.btnSize}
+						sorting={Direction.Descending}
+						onClick={() => handleClickSort('desc')}
+					/>
 				</div>
-				<Button text='Новый массив' extraClass={styles.btnSize} onClick={handleClickNewArr} />
+				<Button
+					text='Новый массив'
+					extraClass={styles.btnSize}
+					onClick={handleClickNewArrWithState}
+				/>
 			</form>
 			<div className={styles.columns}>
-				{arr.map(item => {
+				{arrWithState.map(item => {
 					return <Column key={nanoid()} index={item.number} state={item.state} />;
 				})}
 			</div>
--- src/components/sorting-page/utils/utils.tsx
+++ src/components/sorting-page/utils/utils.tsx
@@ -3,16 +3,20 @@ import { ElementStates } from '../../../types/element-states';
 export const minLen = 3;
 export const maxLen = 17;
 
-export const randomNumber = (min: number, max: number) => {
+export const randomNumber = (min: number, max: number): number => {
 	return Math.floor(min + Math.random() * (max + 1 - min));
 };
 
 // Генерирования последовательности чисел
-export const generateRandomArr = (length: number) => {
+export const generateRandomArr = (length: number): number[] => {
 	return Array.from({ length }, () => {
-		return {
-			number: randomNumber(0, 101),
-			state: ElementStates.Default,
-		};
+		return randomNumber(0, 101);
 	});
 };
+
+export type TArrWithState = {
+	number: number;
+	state: ElementStates;
+};
+
+
