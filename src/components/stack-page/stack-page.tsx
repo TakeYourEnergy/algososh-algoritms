8bf0c81cdc591cd1d4f4b3b7087d49375739eb4b
Alexander <sashashvetsov3@gmail.com>
2022-12-02 20:45:42 +0300

    add class stack + input and buttons

--- src/components/stack-page/stack-page.tsx
+++ src/components/stack-page/stack-page.tsx
@@ -1,10 +1,32 @@
-import React from "react";
-import { SolutionLayout } from "../ui/solution-layout/solution-layout";
+import React, { useState } from 'react';
+import { Button } from '../ui/button/button';
+import { Input } from '../ui/input/input';
+import { SolutionLayout } from '../ui/solution-layout/solution-layout';
+import styles from './stack.module.css';
 
 export const StackPage: React.FC = () => {
-  return (
-    <SolutionLayout title="Стек">
+	const [inputValue, setInputValue] = useState('');
 
-    </SolutionLayout>
-  );
+	const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+		setInputValue(e.target.value);
+	};
+
+	return (
+		<SolutionLayout title='Стек'>
+			<form className={styles.form}>
+				<div className={styles.inputWithAddAndDelete}>
+					<Input
+						maxLength={4}
+						isLimitText={true}
+						value={inputValue}
+						extraClass={styles.input}
+						onChange={handleChange}
+					/>
+					<Button text='Добавить' />
+					<Button text='Удалить' />
+				</div>
+				<Button text='Очистить' />
+			</form>
+		</SolutionLayout>
+	);
 };
