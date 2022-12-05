2b398c28798e9e38407b5b63cdcef1a8da76625b
Alexander <sashashvetsov3@gmail.com>
2022-12-05 20:54:06 +0300

    add class queue

--- src/components/queue-page/queue-page.tsx
+++ src/components/queue-page/queue-page.tsx
@@ -1,10 +1,51 @@
-import React from "react";
-import { SolutionLayout } from "../ui/solution-layout/solution-layout";
+import React, { useState } from 'react';
+import { Button } from '../ui/button/button';
+import { Circle } from '../ui/circle/circle';
+import { Input } from '../ui/input/input';
+import { SolutionLayout } from '../ui/solution-layout/solution-layout';
+import { queue } from './class/queue';
+import styles from './queuePage.module.css';
+import { nanoid } from 'nanoid';
 
 export const QueuePage: React.FC = () => {
-  return (
-    <SolutionLayout title="Очередь">
+	const [inputValue, setInputValue] = useState('');
+	const [queueArr, setQueueArr] = useState(queue.getQueue());
 
-    </SolutionLayout>
-  );
+	console.log(queueArr);
+
+	const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+		setInputValue(e.target.value);
+	};
+
+	const enqueue = async (item: string) => {
+		queue.enqueue(item);
+		setInputValue('');
+		setQueueArr([...queue.getQueue()]);
+	};
+
+	const dequeue = async () => {};
+
+	return (
+		<SolutionLayout title='Очередь'>
+			<form className={styles.form}>
+				<div className={styles.inputWithAddAndDelete}>
+					<Input
+						maxLength={4}
+						isLimitText={true}
+						value={inputValue}
+						extraClass={styles.input}
+						onChange={handleChange}
+					/>
+					<Button text='Добавить' onClick={() => enqueue(inputValue)} />
+					<Button text='Удалить' onClick={dequeue} />
+				</div>
+				<Button text='Очистить' />
+			</form>
+			<div className={styles.queue}>
+				{queueArr.map((item, index) => {
+					return <Circle key={nanoid()} index={index} letter={item} />;
+				})}
+			</div>
+		</SolutionLayout>
+	);
 };
